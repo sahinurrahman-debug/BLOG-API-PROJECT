60a2bb26c40f5d691fd6ac6f759d61f4ddc76f97
sahinurrahman-debug <sahinurrahman1847@gmail.com>
2026-09-18 18:43:47 +0530

    create blog

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI, Request
-from database import engine, Base
-import models
+from fastapi import FastAPI, Depends,HTTPException
+from requests import request
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+import models,schemas
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+#DB Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#home
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Blog API"}
+
+#create blog
+@app.post("/blogs", response_model=schemas.BlogResponse)
+def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, content=blog.content)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+#read all blogs
